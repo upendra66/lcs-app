b1bed9418c307ea2caebeefdcdf6a5704897808f
uyadav509 <UpendraSingh_Yadav@comcast.com>
2021-08-19 15:00:26 -0400

    removing unused method

--- lcs-service/src/main/java/com/assessment/lcsservice/controller/LcsController.java
+++ lcs-service/src/main/java/com/assessment/lcsservice/controller/LcsController.java
@@ -2,7 +2,6 @@ package com.assessment.lcsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import com.assessment.lcsservice.vo.InputData;
 import com.assessment.lcsservice.vo.LongestCommonSubstrings;
 
 @RestController
-@Validated
 public class LcsController {
 	
 	final String SCHEMA_PATH = "classpath:schema.json";
